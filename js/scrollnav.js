e3c2e7d767f87c3049ff0012257eb3e793e0a661
Łukasz Topa <lukasztopa1993@gmail.com>
2018-01-28 16:01:50 +0100

    edit smooth scroll

--- js/scrollnav.js
+++ js/scrollnav.js
@@ -6,7 +6,7 @@ $(function() {
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
       if (target.length) {
         $('html,body').animate({
-          scrollTop: target.offset().top
+          scrollTop: (target.offset().top - 60)
         }, 800);
         
         $('nav ul').removeClass('show');
@@ -14,4 +14,5 @@ $(function() {
       }
     }
   });
+ 
 });
